fd2fe2673f6139f9960362064b7698b93f76d4cc
Łukasz Sereda <lukasz.sereda98@gmail.com>
2020-01-28 21:39:23 +0100

    fixes #27

--- code_structure/create_database.sql
+++ code_structure/create_database.sql
@@ -1,10 +1,10 @@
 CREATE TABLE "User"
     (
      id INTEGER NOT NULL,
-     name VARCHAR NOT NULL,
-     surname VARCHAR NOT NULL,
-     email VARCHAR NOT NULL,
-     password VARCHAR NOT NULL,
+     name VARCHAR(64) NOT NULL,
+     surname VARCHAR(64) NOT NULL,
+     email VARCHAR(64) NOT NULL,
+     password VARCHAR(64) NOT NULL,
      register_date DATETIME NOT NULL,
      policy_accepted BIT NOT NULL,
      CONSTRAINT UserPK PRIMARY KEY(id)
@@ -36,8 +36,8 @@ CREATE TABLE ClientLog
     (
      id INTEGER NOT NULL,
      date DATETIME NOT NULL,
-     category VARCHAR NOT NULL,
-     message VARCHAR,
+     category VARCHAR(32) NOT NULL,
+     message VARCHAR(512),
      client_id INTEGER NOT NULL,
      CONSTRAINT ClientLogPK PRIMARY KEY (id),
      CONSTRAINT ClientLogFK FOREIGN KEY (client_id) REFERENCES Client(user_id)
@@ -59,9 +59,9 @@ GO
 CREATE TABLE Address
     (
      id INTEGER NOT NULL,
-     country VARCHAR NOT NULL,
-     state VARCHAR NOT NULL,
-     street VARCHAR,
+     country VARCHAR(64) NOT NULL,
+     state VARCHAR(64) NOT NULL,
+     street VARCHAR(64),
      number INTEGER NOT NULL,
      flat_number INTEGER,
      zip_code INTEGER NOT NULL,
@@ -84,9 +84,9 @@ GO
 
 CREATE TABLE Blacklist
     (
-     id INTEGER,
-     ip_address VARCHAR,
-     note VARCHAR,
+     id INTEGER NOT NULL,
+     ip_address VARCHAR(32),
+     note VARCHAR(512),
      CONSTRAINT BlacklistPK PRIMARY KEY(id)
     )
 GO
@@ -94,7 +94,7 @@ GO
 CREATE TABLE ProductCategory
     (
      id INTEGER NOT NULL,
-     name VARCHAR NOT NULL,
+     name VARCHAR(64) NOT NULL,
      tax FLOAT NOT NULL,
      CONSTRAINT ProductCategoryPK PRIMARY KEY(id)
     )
@@ -103,8 +103,8 @@ GO
 CREATE TABLE ProductImage
     (
      id INTEGER NOT NULL,
-     name VARCHAR,
-     path VARCHAR NOT NULL,
+     name VARCHAR(64),
+     path VARCHAR(128) NOT NULL,
      width INTEGER NOT NULL,
      height INTEGER NOT NULL,
      CONSTRAINT ProductImagePK PRIMARY KEY (id)
@@ -114,7 +114,7 @@ GO
 CREATE TABLE Product
     (
      id INTEGER NOT NULL,
-     name VARCHAR NOT NULL,
+     name VARCHAR(64) NOT NULL,
      category_id INTEGER NOT NULL,
      image_id INTEGER NOT NULL,
      CONSTRAINT ProductPK PRIMARY KEY(id),
@@ -140,9 +140,9 @@ CREATE TABLE "Order"
      id INTEGER NOT NULL,
      date DATETIME NOT NULL,
      payment_date DATETIME,
-     client_id INTEGER NOT NULL,
+	 client_id INTEGER NOT NULL,
      CONSTRAINT OrderPK PRIMARY KEY (id),
-     CONSTRAINT OrderFKClient FOREIGN KEY (client_id) REFERENCES Client(user_id)
+	 CONSTRAINT OrderFKClient FOREIGN KEY (client_id) REFERENCES Client(user_id)
     )
 GO
 
@@ -150,20 +150,20 @@ CREATE TABLE OrderDetails
     (
      id INTEGER NOT NULL,
      order_id INTEGER NOT NULL,
-     quantity INTEGER NOT NULL,
+	 quantity INTEGER NOT NULL,
      product_id INTEGER NOT NULL,
-     CONSTRAINT OrderDetailsPK PRIMARY KEY (id),
+	 CONSTRAINT OrderDetailsPK PRIMARY KEY (id),
      CONSTRAINT OrderDetailsFKOrder FOREIGN KEY (order_id) REFERENCES "Order"(id),
-     CONSTRAINT OrderDetailsFKProduct FOREIGN KEY (product_id) REFERENCES Product(id)
+	 CONSTRAINT OrderDetailsFKProduct FOREIGN KEY (product_id) REFERENCES Product(id)
     )
 GO
 
 CREATE TABLE OrderHistory
     (
      id INTEGER NOT NULL,
-     status VARCHAR NOT NULL,
+     status VARCHAR(64) NOT NULL,
      date DATETIME NOT NULL,
-     note VARCHAR,
+     note VARCHAR(512),
      order_id INTEGER NOT NULL,
      CONSTRAINT OrderHistoryPK PRIMARY KEY (id),
      CONSTRAINT OrderHistoryFK FOREIGN KEY (order_id) REFERENCES "Order"(id)
@@ -173,7 +173,7 @@ GO
 CREATE TABLE ConversationCategories
     (
      id INTEGER NOT NULL,
-     name VARCHAR NOT NULL,
+     name VARCHAR(64) NOT NULL,
      priority INTEGER NOT NULL,
      CONSTRAINT ConversationCategoriesPK PRIMARY KEY (id)
     )
@@ -190,15 +190,15 @@ CREATE TABLE Conversation
      CONSTRAINT ConversationPK PRIMARY KEY(id),
      CONSTRAINT ConversationFKClient FOREIGN KEY (user_id) REFERENCES Client(user_id),
      CONSTRAINT ConversationFKWorker FOREIGN KEY (worker_id) REFERENCES Worker(user_id),
-     CONSTRAINT ConversationFKCategory FOREIGN KEY (category_id) REFERENCES ConversationCategories(id)
+	 CONSTRAINT ConversationFKCategory FOREIGN KEY (category_id) REFERENCES ConversationCategories(id)
     )
 GO
 
 CREATE TABLE Message
     (
      id INTEGER NOT NULL,
-     author_category VARCHAR NOT NULL,
-     message VARCHAR,
+     author_category VARCHAR(64) NOT NULL,
+     message VARCHAR(512),
      date DATETIME NOT NULL,
      conversation_id INTEGER NOT NULL
      CONSTRAINT MessagePK PRIMARY KEY (id),
@@ -209,9 +209,9 @@ GO
 CREATE TABLE Attachment
     (
      id INTEGER NOT NULL,
-     url VARCHAR NOT NULL,
+     url VARCHAR(64) NOT NULL,
      size INTEGER,
-     data_type VARCHAR,
+     data_type VARCHAR(64),
      message_id INTEGER NOT NULL,
      CONSTRAINT AttachmentPK PRIMARY KEY (id),
      CONSTRAINT AttachmentFK FOREIGN KEY (message_id) REFERENCES Message(id)
