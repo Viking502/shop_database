e6ab3716ab26f80d34dc6b507e8a7b171086d4f6
Łukasz Sereda <lukasz.sereda98@gmail.com>
2020-01-30 00:40:04 +0100

    add clients_activity trigger implementation

---
+++ code_structure/triggers_code/clients_activity.sql
@@ -0,0 +1,52 @@
+IF OBJECT_ID('GetNewOrderPrice', 'fn') IS NOT NULL
+	DROP FUNCTION GetNewOrderPrice
+GO
+
+CREATE FUNCTION GetNewOrderPrice
+(
+    @Quantity INTEGER,
+    @Price DECIMAL
+)
+RETURNS DECIMAL
+AS
+BEGIN
+    RETURN @Quantity * @Price
+END
+GO
+
+IF OBJECT_ID('clients_activity', 'tr') IS NOT NULL
+	DROP TRIGGER clients_activity
+GO
+
+CREATE TRIGGER clients_activity
+ON OrderDetails
+AFTER INSERT
+AS
+BEGIN
+    DECLARE @Quantity INTEGER
+    SET @Quantity = (SELECT quantity FROM inserted)
+    DECLARE @Price INTEGER
+    SET @Price = (SELECT price FROM inserted JOIN Product ON inserted.product_id = Product.id)
+    DECLARE @ClientId INTEGER
+    SET @ClientId = (SELECT client_id
+        FROM inserted JOIN Product ON inserted.product_id = Product.id JOIN [Order] ON inserted.order_id = [Order].id)
+    IF EXISTS
+    (
+        SELECT * FROM ClientActivity WHERE client_id = (SELECT client_id
+            FROM inserted JOIN Product ON inserted.product_id = Product.id JOIN [Order] ON inserted.order_id = [Order].id)
+    )
+    BEGIN
+        UPDATE ClientActivity
+        SET transactions_num = transactions_num + 1
+        WHERE ClientActivity.client_id = @ClientId
+        UPDATE ClientActivity
+        SET total_expenses = total_expenses + dbo.[GetNewOrderPrice](@Quantity, @Price)
+        WHERE ClientActivity.client_id = @ClientId
+    END
+    ELSE
+    BEGIN
+        INSERT INTO ClientActivity
+        VALUES (1, dbo.[GetNewOrderPrice](@Quantity, @Price), @ClientId)
+    END
+END
+
