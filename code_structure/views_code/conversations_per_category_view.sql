1541bd547c6a75700fba09660075a1c00d9474e4
seqre <seqre@disroot.org>
2020-01-30 22:49:27 +0100

    Closes #13

---
+++ code_structure/views_code/conversations_per_category_view.sql
@@ -0,0 +1,11 @@
+IF OBJECT_ID('ConversationsPerCategory', 'v') IS NOT NULL
+	DROP VIEW ConversationsPerCategory
+GO
+
+CREATE VIEW ConversationsPerCategory
+AS
+	SELECT CC.name [Category name], COUNT(*) [Amount]
+	FROM Conversation C
+	JOIN ConversationCategories as CC ON C.id = CC.id
+	GROUP BY CC.name
+GO
