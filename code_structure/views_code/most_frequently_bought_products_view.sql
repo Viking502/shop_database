6959d660ab7f10c49f111b87c91af78b456f0dc0
Łukasz Sereda <lukasz.sereda98@gmail.com>
2020-02-02 00:53:42 +0100

    refactor

--- code_structure/views_code/most_frequently_bought_products_view.sql
+++ code_structure/views_code/most_frequently_bought_products_view.sql
@@ -3,10 +3,9 @@ GO
 
 CREATE VIEW most_frequently_bought_products
 AS
-	SELECT TOP 10 P.id, P.name, SUM(OD.quantity) as quantity
-    FROM [Order] O
-        JOIN OrderDetails OD on O.id = OD.order_id
-        JOIN Product P on OD.product_id = P.id
+	SELECT TOP 10 P.id, P.name, SUM(O.quantity) as quantity
+        FROM OrderDetails O
+        JOIN Product P on product_id = P.id
 	GROUP BY P.id, P.name
     ORDER BY quantity DESC
 GO
