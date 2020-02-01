dfe680ab65e9b99f5f902892e6b3ae164bc1a1b7
Viking502 <wiktor.przadka.1@gmail.com>
2020-02-01 14:13:54 +0100

    refactor code

--- code_structure/triggers_code/auto_response.sql
+++ code_structure/triggers_code/auto_response.sql
@@ -6,11 +6,10 @@ ON Message
 AFTER INSERT
 AS
 	DECLARE @conversation INTEGER = (SELECT DISTINCT conversation_id FROM inserted)
-	DECLARE @last_msg DATETIME = (SELECT MAX(date) FROM Message where conversation_id = @conversation
+	DECLARE @last_msg DATETIME = (SELECT MAX(date) FROM Message WHERE conversation_id = @conversation
 									AND date < (SELECT MIN(date) FROM inserted))
 
-	if DATEDIFF(DAY, @last_msg, CURRENT_TIMESTAMP) > 1 OR @last_msg IS NULL
+	IF DATEDIFF(DAY, @last_msg, CURRENT_TIMESTAMP) > 1 OR @last_msg IS NULL
 		INSERT INTO Message (author_category, message, date, conversation_id)
 		VALUES('Bot', 'Hello, our consultant will contact you as soon as possible.', CURRENT_TIMESTAMP, @conversation)
-go
-
+GO
