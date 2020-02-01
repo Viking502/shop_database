665e3937400bb7a4e12b0871f3adc8d5d3afdc94
Łukasz Sereda <lukasz.sereda98@gmail.com>
2020-02-01 18:36:40 +0100

    ?

--- code_structure/triggers_code/clients_activity.sql
+++ code_structure/triggers_code/clients_activity.sql
@@ -7,30 +7,42 @@ ON OrderDetails
 AFTER INSERT
 AS
 BEGIN
-    DECLARE @Quantity INTEGER
-    SET @Quantity = (SELECT quantity FROM inserted)
-    DECLARE @Price INTEGER
-    SET @Price = (SELECT price FROM inserted JOIN Product ON inserted.product_id = Product.id)
-    DECLARE @ClientId INTEGER
-    SET @ClientId = (SELECT client_id
+    DECLARE iterator CURSOR
+		FOR SELECT * FROM inserted
+    DECLARE @id INTEGER
+    DECLARE @order_id INTEGER
+    DECLARE @quantity INTEGER
+    DECLARE @product_id INTEGER
+    OPEN iterator
+    FETCH iterator INTO @id, @order_id, @quantity, @product_id
+    WHILE @@FETCH_STATUS = 0
+    BEGIN
+        DECLARE @price DECIMAL (10,2)
+        SET @Price = (SELECT price FROM inserted JOIN Product ON inserted.product_id = Product.id)
+        DECLARE @client_id INTEGER
+        SET @client_id = (SELECT client_id
         FROM inserted JOIN Product ON inserted.product_id = Product.id JOIN [Order] ON inserted.order_id = [Order].id)
-    IF EXISTS
-    (
-        SELECT * FROM ClientActivity WHERE client_id = (SELECT client_id
-            FROM inserted JOIN Product ON inserted.product_id = Product.id JOIN [Order] ON inserted.order_id = [Order].id)
-    )
-    BEGIN
-        UPDATE ClientActivity
-        SET transactions_num = transactions_num + 1
-        WHERE ClientActivity.client_id = @ClientId
-        UPDATE ClientActivity
-        SET total_expenses = total_expenses + @Quantity * @Price
-        WHERE ClientActivity.client_id = @ClientId
+        IF EXISTS
+        (
+            SELECT * FROM ClientActivity WHERE client_id = (SELECT client_id
+                FROM inserted JOIN Product ON inserted.product_id = Product.id JOIN [Order] ON inserted.order_id = [Order].id)
+        )
+        BEGIN
+            UPDATE ClientActivity
+            SET transactions_num = transactions_num + 1
+            WHERE ClientActivity.client_id = @client_id
+            UPDATE ClientActivity
+            SET total_expenses = total_expenses + @Quantity * @Price
+            WHERE ClientActivity.client_id = @client_id
+        END
+        ELSE
+        BEGIN
+            INSERT INTO ClientActivity
+            VALUES (1, @Quantity * @Price, @client_id)
+        END
+        FETCH iterator INTO @id, @order_id, @quantity, @product_id
     END
-    ELSE
-    BEGIN
-        INSERT INTO ClientActivity
-        VALUES (1, @Quantity * @Price, @ClientId)
-    END
+    CLOSE iterator
+    DEALLOCATE iterator
 END
-
+GO
