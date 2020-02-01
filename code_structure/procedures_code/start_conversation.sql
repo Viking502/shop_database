1b922d835f15159b63479b60071aca432c34977c
Viking502 <wiktor.przadka.1@gmail.com>
2020-02-01 18:34:46 +0100

    add message logic

--- code_structure/procedures_code/start_conversation.sql
+++ code_structure/procedures_code/start_conversation.sql
@@ -6,22 +6,25 @@ AS
 BEGIN
 
 	DECLARE @worker INTEGER = NULL
-	DECLARE @message VARCHAR(32) = 'Your conversation has been started'
+	DECLARE @message VARCHAR(128) = 'Your conversation has been started'
 
 	DECLARE @online_worker INTEGER = (SELECT TOP 1 user_id FROM Worker WHERE is_online = 1)
 	IF @online_worker IS NOT NULL
 		BEGIN
 		SET @worker = @online_worker
-		SET @message += ' one of our consultant will response you'
+		SET @message += ' and one of our consultant will respond you shortly'
 		END
 	ELSE
 		BEGIN
 		SET @worker = (SELECT TOP 1 user_id FROM Worker)
-		SET @message += ' but no administrator is online. Someone will response for your problem soon'
+		SET @message += ', but no administrator is online. Someone will respond for your problem soon'
 		END
 
 	INSERT INTO Conversation (start_date, is_closed, category_id, user_id, worker_id)
 	VALUES(CURRENT_TIMESTAMP, 0, @category_id, @user_id, @worker)
+
+	INSERT INTO Message (author_category, message, date, conversation_id)
+	VALUES('Bot', @message, CURRENT_TIMESTAMP, @@IDENTITY)
 
 END
 GO
