aee493f51ba4ba935be94c81d424eec1cdfb32cc
Viking502 <wiktor.przadka.1@gmail.com>
2020-02-01 20:20:15 +0100

    refactor code

--- code_structure/views_code/generate_balance.sql
+++ code_structure/views_code/generate_balance.sql
@@ -5,23 +5,23 @@ CREATE VIEW generate_balance
 AS
 BEGIN
 
-WITH
-income as(
-	SELECT dbo.date_part(payment_date, 3) as "month", SUM(price * quantity) as orders
-	FROM Product AS p
-	JOIN OrderDetails AS od 
-		ON p.id = od.product_id
-	JOIN "Order" AS o
-		ON od.order_id = o.id
-	GROUP BY dbo.date_part(payment_date, 3)
-),
-loss as(
-	SELECT SUM(salary) as salary
-	FROM Worker
-)
+	WITH
+	income as(
+		SELECT dbo.date_part(payment_date, 3) as "month", SUM(price * quantity) as orders
+		FROM Product AS p
+		JOIN OrderDetails AS od 
+			ON p.id = od.product_id
+		JOIN "Order" AS o
+			ON od.order_id = o.id
+		GROUP BY dbo.date_part(payment_date, 3)
+	),
+	loss as(
+		SELECT SUM(salary) as salary
+		FROM Worker
+	)
 
-SELECT "month", orders - salary
-FROM income, loss
+	SELECT "month", orders - salary
+	FROM income, loss
 
 END
 GO
