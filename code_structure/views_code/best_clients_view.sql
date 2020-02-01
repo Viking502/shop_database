5a9b51d9abca93c086dadda866bf0631a43d56a2
Lukasz Sereda <lukasz.sereda98@gmail.com>
2020-02-01 17:18:18 +0100

    refactor

--- code_structure/views_code/best_clients_view.sql
+++ code_structure/views_code/best_clients_view.sql
@@ -3,10 +3,9 @@ GO
 
 CREATE VIEW best_clients
 AS
-	SELECT TOP 10 CONCAT(U.name, ' ', U.surname) AS ClientName, total_expenses AS TotalExpenses
+	SELECT TOP 10 CONCAT(U.name, ' ', U.surname) AS ClientName, A.total_expenses AS TotalExpenses
 	FROM Client C
 	JOIN ClientActivity A ON C.user_id = A.client_id
 	JOIN "User" U ON C.user_id = U.id
-	GROUP BY CONCAT(U.name, ' ', U.surname), total_expenses
 	ORDER BY total_expenses DESC
 GO
