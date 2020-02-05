a4a78b9352e42736401bdce9259974ccbd534b20
seqre <seqre@disroot.org>
2020-02-05 15:18:51 +0100

    Fixes according to review

--- code_structure/functions_code/clients_per_month.sql
+++ code_structure/functions_code/clients_per_month.sql
@@ -3,7 +3,8 @@ GO
 
 CREATE FUNCTION ClientsPerMonth
 (
-        @month INTEGER
+        @month INTEGER,
+        @year INTEGER
 )
 RETURNS INTEGER
 AS
@@ -11,7 +12,7 @@ BEGIN
     DECLARE @val INTEGER = (SELECT COUNT(DISTINCT C.user_id)
                             FROM Client C
                             JOIN [Order] O on C.user_id = O.client_id
-                            WHERE DATEPART(MONTH, O.date) = @month)
+                            WHERE DATEPART(MONTH, O.date) = @month AND DATEPART(YEAR, O.date) = @year)
 	RETURN  @val
 END
 GO
