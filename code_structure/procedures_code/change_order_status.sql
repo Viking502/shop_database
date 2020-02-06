6cbb71f72ba2bc6c8d7d877306f05df1aa81b888
Łukasz Sereda <lukasz.sereda98@gmail.com>
2020-02-06 11:47:39 +0100

    refactor procedure and try to resolve conflict

--- code_structure/procedures_code/change_order_status.sql
+++ code_structure/procedures_code/change_order_status.sql
@@ -9,14 +9,12 @@ CREATE PROCEDURE change_order_status
 )
 AS
 BEGIN
-    DECLARE @last_status_id INTEGER
-    SET @last_status_id = (SELECT OrderHistory.id
+    DECLARE @last_status VARCHAR(64)
+    SET @last_status = (SELECT status
         FROM OrderHistory
         WHERE order_id = @id AND id = (SELECT MAX(id) FROM OrderHistory
                                         WHERE order_id = @id GROUP BY order_id)
-        GROUP BY OrderHistory.id)
-    DECLARE @last_status VARCHAR(64)
-    SET @last_status = (SELECT status FROM OrderHistory WHERE id = @last_status_id)
+        GROUP BY status)
     IF @last_status != @status
     BEGIN
         INSERT INTO OrderHistory
